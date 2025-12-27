c89b2fb18dcd64505e2f4e9dc69617b66bfb4f25
MHmdHajj <mhmd.10hajj@gmail.com>
2025-12-28 00:24:04 +0200

    Save progress

--- src/assets/components/Todo.jsx
+++ src/assets/components/Todo.jsx
@@ -2,36 +2,105 @@ import { useState } from "react"
 function Todo (){
 const [todos,setTodos] = useState([]);
 const [input, setInput]= useState("");
-const addtodo = ()=>{
-if(!input.trim()) return;
-setTodos([...todos,input]);
-setInput("");
+const [hours,setHours]= useState("0");
+const [minutes,setMinutes] = useState("0");
+
+const addtodo = (e)=>{
+    e.preventDefault();
+    const h = Number(hours);
+    if (!Number.isFinite(h) || h<0 ) {
+       alert("Hours must be >= 0.");
+        return;
+    }
+    const m = Number(minutes);
+    if (!Number.isFinite(m) || m < 0 || m > 59) {
+       alert("Minutes must be between 0 and 59.");
+        return;
+    }
+    if (h === 0 && m === 0) {
+        alert("Please set a duration (hours or minutes).");
+        return;
+    }
+    if(!input.trim()){
+        alert("Please enter a todo")
+        return;
+    }
+    const duration = ((h*60+m)*60*1000);
+    const creatAt = Date.now();
+    const dueTO = creatAt + duration;
+    const NewTodo = {
+      id : crypto.randomUUID(),
+      value: input,
+      creatAt,
+      dueTO,
+    }
+    if(!input.trim()) return;
+    setTodos([...todos,NewTodo]);
+    setInput("");
+    setHours("0");
+    setMinutes("0");
 }
 const deletetodo = (indextodelete) =>{
-    setTodos(todos.filter((_,index) => index !== indextodelete))
+    setTodos(todos.filter((todo) => todo.id !== indextodelete))
 }
 
 return(
-    <div>
+    <div className="parent">
+        <div className="enter">
+        <form onSubmit={addtodo}>
         <input 
         value={input}
         onChange={(e)=>setInput(e.target.value)}
         placeholder='type a todo...'
         />
-        <button onClick={addtodo}>Add</button>
-        {todos.length==0 ? (<p>no todos</p>)
-        :(
-        <ul className="main">
-            {todos.map((todo,ind)=>
-            (<li key={ind} className="insidemain">
-                {todo}
-                <button onClick={()=>deletetodo(ind)}>
-                    Delete
-                </button>
-            </li>)
-            )}
-        </ul>)
-        }
+        <label className="l1">
+            Hours: 
+            <input 
+            type="number"
+            min={0}
+            max={24}
+            step={1}
+            value={hours}
+            onChange={(e)=>setHours(e.target.value)} />
+        </label>
+        <label className="l1">
+            Minutes: 
+            <input  
+            type="number"
+            min={0}
+            max={59}
+            step={1}
+            value={minutes}
+            onChange={(e)=>setMinutes(e.target.value)}
+            />
+        </label>
+        <button type="Submit">Add</button>
+        </form>
+        </div>
+        <div className="todos">
+            {
+                todos.length==0 ? (<p>no todos</p>)
+                :(
+                <ul className="main">
+                    {todos.map((todo)=>
+                    (<li key={todo.id} className="insidemain">
+                        <span>
+                            {todo.value}
+                            <br />
+                            starts at: {new Date(todo.creatAt).toLocaleString()}
+                            <br />
+                            ends at: {new Date(todo.dueTO).toLocaleString()}
+                        </span>
+                        <br />
+                        <button onClick={()=>deletetodo(todo.id)}>
+                            Delete
+                        </button>
+                    </li>)
+                    )}
+                </ul>)
+        
+            }
+        </div>
 
 
 
